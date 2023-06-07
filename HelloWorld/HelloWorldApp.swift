5a6a0693039529778ace91b1eb539429f3524e9a
yjyuwisely <yjyuwisely@gmail.com>
2023-06-08 04:21:10 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by YEONGJIN YU on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
